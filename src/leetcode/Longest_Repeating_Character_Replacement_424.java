965c49d04dd3cc42fd4a0e1c2441f2144263dfd6
kkzhang <15386423830@163.com>
2019-10-18 11:15:06 +0800

    modify leetcode_424

--- src/leetcode/Longest_Repeating_Character_Replacement_424.java
+++ src/leetcode/Longest_Repeating_Character_Replacement_424.java
@@ -64,6 +64,27 @@ public class Longest_Repeating_Character_Replacement_424 {
         return result;
     }
 
+    // 解法基本一致，但是更简洁
+    // There's no edge case for this question. The initial step is to extend the window to its limit, that is, the longest we can get to with maximum number of modifications.
+    // Until then the variable start will remain at 0.
+    //
+    //Then as end increase, the whole substring from 0 to end will violate the rule, so we need to update start accordingly (slide the window).
+    // We move start to the right until the whole string satisfy the constraint again. Then each time we reach such situation, we update our max length.
+    public int characterReplacement_2(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         System.out.println(characterReplacement("AABABBA", 1));
     }
