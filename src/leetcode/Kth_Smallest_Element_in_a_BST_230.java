c7a822aa4ad2e683af66d2dc1ab8ec667151740c
张凯凯 <zhangkaikai@bytedance.com>
2020-03-12 14:17:33 +0800

    add no.230 & 671

--- src/leetcode/Kth_Smallest_Element_in_a_BST_230.java
+++ src/leetcode/Kth_Smallest_Element_in_a_BST_230.java
@@ -1,4 +1,41 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kth_Smallest_Element_in_a_BST_230 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // 因为是二叉查找树，所以对这个树进行中序遍历就能得到一个有序的数组
+    // 相应的就比较容易找到第k个大小的值
+    public static int kthSmallest(TreeNode root, int k) {
+        List<Integer> travel = new ArrayList<>();
+        inOrderTravel(root, travel);
+        return travel.get(k);
+    }
+
+    public static void inOrderTravel(TreeNode root, List<Integer> travel) {
+        if (root == null) return;
+        inOrderTravel(root.left, travel);
+        travel.add(root.val);
+        inOrderTravel(root.right, travel);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(3);
+        root.left = left;
+        root.right = right;
+        kthSmallest(root, 1);
+    }
+
 }
--- src/leetcode/Second_Minimum_Node_In_a_Binary_Tree_671.java
+++ src/leetcode/Second_Minimum_Node_In_a_Binary_Tree_671.java
@@ -1,4 +1,45 @@
 package leetcode;
 
 public class Second_Minimum_Node_In_a_Binary_Tree_671 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // 其实该二叉树类似于最小堆，根结点即为值最小的节点，只需要从根结点往下遍历并找到比根结点大的第一个值即可
+    // 最后一个case不通过
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return -1;
+        if (root.left != null && root.val < root.left.val) return root.left.val;
+        if (root.right != null && root.val < root.right.val) return root.right.val;
+        // 此时说明根结点的值与左右两个字节点的值相等
+        // 继续向下遍历比较
+        int left = findSecondMinimumValue(root.left);
+        int right = findSecondMinimumValue(root.right);
+
+        if ((left == right) && (right == root.val)) return -1;
+        if (left != -1 && right != -1) {
+            return Math.min(left, right);
+        } else if (left != -1) {
+            return Math.max(root.val, left);
+        } else if (right != -1) {
+            return Math.max(root.val, right);
+        } else {
+            return -1;
+        }
+    }
+
+    public int findSecondMin(TreeNode root) {
+        if (root.left == null) return -1;
+
+        int l = root.left.val == root.val ? findSecondMinimumValue(root.left) : root.left.val;
+        int r = root.right.val == root.val ? findSecondMinimumValue(root.right) : root.right.val;
+
+        return l == -1 || r == -1 ? Math.max(l, r) : Math.min(l, r);
+    }
 }
